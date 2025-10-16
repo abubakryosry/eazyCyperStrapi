0ffc279cf2e6b170ddcec7f0475951cd735236a4
Abubakr Yosry <abubakryosryabubakr@gmail.com>
2025-10-16 14:53:53 +0400

    Fix database.js for PostgreSQL on Render

--- config/database.ts
+++ config/database.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   connection: {
-    client: env("DATABASE_CLIENT", "sqlite"),
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: env("DATABASE_CLIENT") === "sqlite"
       ? {
           filename: env("DATABASE_FILENAME", ".tmp/data.db"),
@@ -8,10 +8,10 @@ export default ({ env }) => ({
       : {
           host: env("DATABASE_HOST", "127.0.0.1"),
           port: env.int("DATABASE_PORT", 5432),
-          database: env("DATABASE_NAME", "strapi"),
-          user: env("DATABASE_USERNAME", "strapi"),
-          password: env("DATABASE_PASSWORD", "strapi"),
-          ssl: env.bool("DATABASE_SSL", false) ? { rejectUnauthorized: false } : false,
+          database: env("DATABASE_NAME", "my_eazystrapi_db"),
+          user: env("DATABASE_USERNAME", "my_eazystrapi_db_user"),
+          password: env("DATABASE_PASSWORD", "10zif9zwh41LNFy8juYsGFppIGHaH1oe"),
+          ssl: env.bool("DATABASE_SSL", true) ? { rejectUnauthorized: false } : false,
         },
     useNullAsDefault: true,
   },
